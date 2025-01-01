d1e74760f43dc32eb93531d8f20f4c92391e2535
digitrev <digitrev@gmail.com>
2025-01-01 13:22:05 -0500

    HTML Node

--- src/main.py
+++ src/main.py
@@ -1,7 +1,10 @@
 from textnode import TextNode, TextType
+from htmlnode import HTMLNode
 
 def main():
-    t = TextNode("fdsajklfdsa", TextType.BOLD, "example.com")
-    print(t)
+    n = HTMLNode(props = {"href": "google.ca", "alt": "hi"})
+    print(n)
+    print(n.props_to_html())
+    print(n.props_to_html()[0])
 
 main()
---
+++ src/test_htmlnode.py
@@ -0,0 +1,28 @@
+import unittest
+
+from htmlnode import HTMLNode
+
+class TestHTMLNode(unittest.TestCase):
+    def test_gen_tag(self):
+        node = HTMLNode(tag="t1")
+        self.assertEqual(node.tag, "t1")
+
+    def test_gen_value(self):
+        node = HTMLNode(value="v1")
+        self.assertEqual(node.value, "v1")
+
+    def test_gen_children(self):
+        children = [HTMLNode(), HTMLNode()]
+        node = HTMLNode(children=children)
+        self.assertEqual(len(node.children), 2)
+
+    def test_gen_props(self):
+        node = HTMLNode(props={"href": "google.ca", "alt": "hi"})
+        self.assertEqual(node.props, {"href": "google.ca", "alt": "hi"})
+
+    def test_props_to_html(self):
+        node = HTMLNode(props={"href": "https://www.google.com", "target": "_blank"})
+        self.assertEqual(node.props_to_html(), ' href="https://www.google.com" target="_blank"')
+
+if __name__ == "__main__":
+    unittest.main()
